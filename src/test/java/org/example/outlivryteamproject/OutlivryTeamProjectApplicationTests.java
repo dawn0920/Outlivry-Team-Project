0066a337c42a066bf1c59d7bd63cdda3a25bf5fe
dawn0920 <saoul0920@gmail.com>
2025-04-22 15:09:30 +0900

    first commit

---
+++ src/test/java/org/example/outlivryteamproject/OutlivryTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.outlivryteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlivryTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

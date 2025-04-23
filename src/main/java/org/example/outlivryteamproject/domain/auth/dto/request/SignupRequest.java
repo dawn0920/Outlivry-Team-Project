455d46075218aeee0c8e9af0bb82deafc767561c
dawn0920 <saoul0920@gmail.com>
2025-04-23 20:14:04 +0900

    Feat: 회원가입 dto 생성

---
+++ src/main/java/org/example/outlivryteamproject/domain/auth/dto/request/SignupRequest.java
@@ -0,0 +1,31 @@
+package org.example.outlivryteamproject.domain.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "비밀번호는 영문과 숫자를 포함한 8자 이상이어야 합니다.")
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String  phone;
+
+    @NotBlank
+    private String birth;
+}

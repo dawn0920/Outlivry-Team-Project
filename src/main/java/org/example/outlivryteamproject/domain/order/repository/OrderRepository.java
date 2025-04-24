6c9569745046c22ddf0a02c22d116c7091605878
chaewon9999 <w0729@naver.com>
2025-04-24 14:55:05 +0900

    feat : 주문 생성 구현 완료
    테스트 못함

--- src/main/java/org/example/outlivryteamproject/domain/order/repository/OrderRepository.java
+++ src/main/java/org/example/outlivryteamproject/domain/order/repository/OrderRepository.java
@@ -4,4 +4,5 @@ import org.example.outlivryteamproject.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }

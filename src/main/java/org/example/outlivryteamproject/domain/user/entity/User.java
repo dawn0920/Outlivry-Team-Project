7cba857397a32750d5605d9c7b743a30380b9f2d
dawn0920 <saoul0920@gmail.com>
2025-04-25 21:26:56 +0900

    Feat: user에서 test 하기 애매한 코드 주석처리 및 LoginType 추가

--- src/main/java/org/example/outlivryteamproject/domain/user/entity/User.java
+++ src/main/java/org/example/outlivryteamproject/domain/user/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.example.outlivryteamproject.common.BaseEntity;
+import org.example.outlivryteamproject.domain.user.enums.LoginType;
 import org.example.outlivryteamproject.domain.user.enums.UserRole;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -29,7 +30,7 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String password;
 
     @Column(nullable = false)
@@ -41,7 +42,7 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String phone;
 
-    @Column(nullable = false)
+
     private String birth;
 
     @Column
@@ -49,6 +50,9 @@ public class User extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
+
+    @Enumerated(EnumType.STRING)
+    private LoginType loginType;
 
 //    // cascade = CascadeType.ALL USER 삭제시 같이 store도 처리 수정
 //    // orphanRemoval = true (user 에서 store를 제거시 DB에서도 삭제)

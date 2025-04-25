182ebbc1a4484dfe0584076fdf26eb6648b6380a
dawn0920 <saoul0920@gmail.com>
2025-04-25 21:36:21 +0900

    Feat: naver 소셜 로그인 controller 추가

--- src/main/java/org/example/outlivryteamproject/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/outlivryteamproject/domain/auth/controller/AuthController.java
@@ -4,13 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.outlivryteamproject.domain.auth.dto.request.SigninRequest;
 import org.example.outlivryteamproject.domain.auth.dto.request.SignupRequest;
+import org.example.outlivryteamproject.domain.auth.dto.response.NaverUserInfoReponse;
 import org.example.outlivryteamproject.domain.auth.dto.response.SigninResponse;
 import org.example.outlivryteamproject.domain.auth.dto.response.SignupResponse;
 import org.example.outlivryteamproject.domain.auth.service.AuthService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -33,9 +32,20 @@ public class AuthController {
         return authService.ownersignup(request);
     }
 
+    // 로그인
     @PostMapping("/signin")
     public SigninResponse signin(
             @Valid @RequestBody SigninRequest request) {
         return authService.signin(request);
     }
+
+    // 네이버 소셜 로그인
+    @GetMapping("/naver/callback")
+    public SignupResponse naversignin(
+            @RequestParam String code) {
+        return authService.naverLogin(code);
+    }
+
+    // https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=oWjzWkfgNEUAX2nl9Yy1&redirect_uri=http://localhost:8080/auth/naver/callback
+    // https://nid.naver.com/oauth2.0/token?response_type=code&client_id=oWjzWkfgNEUAX2nl9Yy1&redirect_uri=http://localhost:8080/auth/naver/callback
 }

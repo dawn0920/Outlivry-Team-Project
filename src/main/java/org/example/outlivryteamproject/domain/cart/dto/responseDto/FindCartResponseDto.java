e3a055f82e40dca62c7fd8a946b5580346d06314
chaewon9999 <w0729@naver.com>
2025-04-25 18:56:08 +0900

    fix : 장바구니 FindCartResponseDto 수정
    장바구니 조회 시 수량, menuId 보이게 수정

--- src/main/java/org/example/outlivryteamproject/domain/cart/dto/responseDto/FindCartResponseDto.java
+++ src/main/java/org/example/outlivryteamproject/domain/cart/dto/responseDto/FindCartResponseDto.java
@@ -8,13 +8,19 @@ public class FindCartResponseDto {
 
     private final Long cartId;
 
+    private final Long menuId;
+
     private final String menuName;
 
     private final Integer price;
 
+    private final Integer quantity;
+
     public FindCartResponseDto(Cart cart) {
         this.cartId = cart.getCartId();
+        this.menuId = cart.getMenu().getMenuId();
         this.menuName = cart.getMenuName();
         this.price = cart.getPrice();
+        this.quantity = cart.getQuantity();
     }
 }
